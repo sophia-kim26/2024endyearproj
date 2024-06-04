f7e42fcce5294051a3625cdb004c0af3f8bbee8b
lejubi <junalee515@gmail.com>
2024-06-04 14:08:20 -0400

    struggling

--- Assets/Scenes/Scripts/arrowMovement.cs
+++ Assets/Scenes/Scripts/arrowMovement.cs
@@ -6,6 +6,10 @@ public class arrowMovement : MonoBehaviour
 {
 
     private Vector3 dir = Vector3.up;
+    public GameObject left;
+    public GameObject right;
+    public GameObject up;
+    public GameObject down;
     private float speed = 1.0f;
 
     // maybe we need to add SerializeField stuff here? idk what it actually does
@@ -13,7 +17,7 @@ public class arrowMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(left, new Vector3(0, -1, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
