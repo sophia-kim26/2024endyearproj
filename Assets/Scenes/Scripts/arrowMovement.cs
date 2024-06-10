183891e1df7eba088456d98104c9b3a36b839230
lejubi <junalee515@gmail.com>
2024-06-10 15:53:52 -0400

    pause button

--- Assets/Scenes/Scripts/arrowMovement.cs
+++ Assets/Scenes/Scripts/arrowMovement.cs
@@ -1,51 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class arrowMovement : MonoBehaviour
 {
-
     private float speed = 5.0f;
     private bool paused = false;
-    public GameObject left;
-    public GameObject right;
-    public GameObject up;
-    public GameObject down;
-    private ButtonAction buttonActionScript;
-    Vector3 direction = Vector3.up;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        buttonActionScript = FindObjectOfType<ButtonAction>();
-        if (buttonActionScript == null) {
-            Debug.LogError("ButtonAction script not found in the scene.");
-        }
-
-
-        if (left == null || right == null || up == null || down == null)
-        {
-            Debug.LogError("Please assign all arrow GameObjects in the Inspector.");
-        }
-
-        // Debug.Log("test log in arrowMovement");
-        paused = false;
-        // Instantiate(left, new Vector3(0, -1, 0), Quaternion.identity);
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        paused = buttonActionScript.paused;
-        Debug.Log("Paused: " + paused);
+        if (!paused)
+        {
+            // Move the arrow
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
 
-        direction = Vector3.up;
-        if (paused) direction = Vector3.zero;
-
-        if (left != null) left.transform.Translate(direction * speed * Time.deltaTime);
-        if (right != null) right.transform.Translate(direction * speed * Time.deltaTime);
-        if (up != null) up.transform.Translate(direction * speed * Time.deltaTime);
-        if (down != null) down.transform.Translate(direction * speed * Time.deltaTime);  
+    public void SetPaused(bool isPaused)
+    {
+        paused = isPaused;
     }
 }
