aed8262b461bd1e61e4d8be6cef9fd62558528f6
sophia kim <sophkim26@bergen.org>
2024-06-10 23:11:00 -0400

    start button works

--- Assets/Scenes/Scripts/PauseButtonAction.cs
+++ Assets/Scenes/Scripts/PauseButtonAction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;  
 using UnityEngine;  
   
-public class ButtonAction : MonoBehaviour  
+public class PauseButtonAction : MonoBehaviour  
 {  
     public bool paused { get; private set; }
     
