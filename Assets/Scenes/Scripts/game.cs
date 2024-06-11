964d9fba76d043fe6f8b847ae0a1125e48e682d0
lejubi <junalee515@gmail.com>
2024-06-11 07:49:41 -0400

    a

--- Assets/Scenes/Scripts/game.cs
+++ Assets/Scenes/Scripts/game.cs
@@ -18,49 +18,49 @@ public class game : MonoBehaviour
         // also in theory if we wanted to increase speed we could they would all just look like scary equations like the ones in the beginning
 
         // BUM BUM BUM BUM
-        addUp(3.36f-(3.36f-(-10.5f)/5f*7f));
-        addRight(3.36f-(3.36f-(-11.6f)/5f*7f));
-        addUp(3.36f-(3.36f-(-12.8f)/5f*7f));
-        addRight(3.36f-(3.36f-(-13.9f)/5f*7f));
+        addUp(3.36f-(3.36f-(-10.5f)/5f*7f)+5f);
+        addRight(3.36f-(3.36f-(-11.5f)/5f*7f)+5f);
+        addUp(3.36f-(3.36f-(-12.5f)/5f*7f)+5f);
+        addRight(3.36f-(3.36f-(-13.5f)/5f*7f)+5f);
 
         // baDaDaDa
-        addRight(3.36f-(3.36f-(-27.5f)/5f*7f));
-        addLeft(3.36f-(3.36f-(-28f)/5f*7f));
-        addRight(3.36f-(3.36f-(-28.5f)/5f*7f));
-        addLeft(3.36f-(3.36f-(-29f)/5f*7f));
+        addRight(3.36f-(3.36f-(-27.5f)/5f*7f)+6f);
+        addLeft(3.36f-(3.36f-(-28f)/5f*7f)+6f);
+        addRight(3.36f-(3.36f-(-28.5f)/5f*7f)+6f);
+        addLeft(3.36f-(3.36f-(-29f)/5f*7f)+6f);
 
         // badadada
         // badadada!!
-        addUp(3.36f-(3.36f-(-34.5f)/5f*7f));
-        addLeft(3.36f-(3.36f-(-35f)/5f*7f));
-        addUp(3.36f-(3.36f-(-35.5f)/5f*7f));
-        addLeft(3.36f-(3.36f-(-36f)/5f*7f));
+        addUp(3.36f-(3.36f-(-34.5f)/5f*7f)+8f);
+        addLeft(3.36f-(3.36f-(-35f)/5f*7f)+8f);
+        addUp(3.36f-(3.36f-(-35.5f)/5f*7f)+8f);
+        addLeft(3.36f-(3.36f-(-36f)/5f*7f)+8f);
 
         // baDaDaDa
-        addLeft(3.36f-(3.36f-(-41.5f)/5f*7f));
-        addRight(3.36f-(3.36f-(-42f)/5f*7f));
-        addLeft(3.36f-(3.36f-(-42.5f)/5f*7f));
-        addRight(3.36f-(3.36f-(-43f)/5f*7f));
+        addLeft(3.36f-(3.36f-(-41.5f)/5f*7f)+8f);
+        addRight(3.36f-(3.36f-(-42f)/5f*7f)+8f);
+        addLeft(3.36f-(3.36f-(-42.5f)/5f*7f)+8f);
+        addRight(3.36f-(3.36f-(-43f)/5f*7f)+8f);
 
         // badadada
         // badadada!!
-        addLeft(-64.5f);
-        addUp(-65.2f);
-        addLeft(-65.9f);
-        addUp(-66.6f);
+        addLeft(-64.5f+8f);
+        addUp(-65.2f+8f);
+        addLeft(-65.9f+8f);
+        addUp(-66.6f+8f);
 
         // dadadodum
-        addUp(-71.5f);
-        addRight(-72.2f);
-        addUp(-72.9f);
-        addRight(-73.6f);
+        addUp(-71.5f+8f);
+        addRight(-72.2f+8f);
+        addUp(-72.9f+8f);
+        addRight(-73.6f+8f);
 
         // adadadodumm
-        addLeft(-78.5f);
-        addUp(-79.2f);
-        addLeft(-79.9f);
-        addUp(-80.6f);
-        addLeft(-81.3f);
+        addLeft(-78.5f+8f);
+        addUp(-79.2f+8f);
+        addLeft(-79.9f+8f);
+        addUp(-80.6f+8f);
+        addLeft(-81.3f+8f);
 
         // adadadodummm
         addRight(-85.4f);
