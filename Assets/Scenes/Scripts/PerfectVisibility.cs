52de7f8dc0862cf851e2aff87931ac51e50ffb65
sylviaalee <sylvieelee@gmail.com>
2024-06-12 14:07:44 -0400

    perfect

---
+++ Assets/Scenes/Scripts/PerfectVisibility.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+private SpriteRenderer sprender;
+public void on(){
+    sprender.enabled = true;
+}
+public void off(){
+    sprender.enabled = false;
+}
