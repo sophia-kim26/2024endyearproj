be72c6f140d780e3283764d28f9ef3ae3cdbceac
lejubi <junalee515@gmail.com>
2024-06-13 00:03:26 -0400

    hgjk

--- Assets/Scenes/Scripts/ScreenFlash.cs
+++ Assets/Scenes/Scripts/ScreenFlash.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ScreenFlash : MonoBehaviour
 {
+    public Image flashImage;
+    public float flashDuration = 0.5f;
+    public Color flashColor = Color.white;
 
     private Animator anim;
     // Start is called before the first frame update
@@ -15,11 +18,34 @@ public class ScreenFlash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(FlashRoutine());
     }
 
     public void Flash() 
     {
-        anim.SetTrigger("flash");
+        float elapsedTime = 0f;
+
+        flashImage.color = flashColor;
+
+        while (elapsedTime < flashDuration / 2)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(0, 1, elapsedTime / (flashDuration / 2));
+            flashImage.color = new Color(flashColor.r, flashColor.g, flashColor.b, alpha);
+            yield return null;
+        }
+
+        elapsedTime = 0f;
+        // Fade out
+        while (elapsedTime < flashDuration / 2)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, elapsedTime / (flashDuration / 2));
+            flashImage.color = new Color(flashColor.r, flashColor.g, flashColor.b, alpha);
+            yield return null;
+        }
+
+        // Ensure the image is fully transparent at the end
+        flashImage.color = new Color(flashColor.r, flashColor.g, flashColor.b, 0);
     }
 }
