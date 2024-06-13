e2b19feb517d3ce3f0e2d8de7c1876defffa0f8a
sylviaalee <sylvieelee@gmail.com>
2024-06-13 07:54:12 -0400

    adsfas

--- Assets/Scenes/Scripts/endScript.cs
+++ Assets/Scenes/Scripts/endScript.cs
@@ -17,13 +17,13 @@ public class EndScript : MonoBehaviour
     void Start(){
         endscore = PlayerPrefs.GetInt("Score");
         scoretext.text = "SCORE: " + endscore;
-        restartButton.onClick.AddListener(RestartGame);
+        //restartButton.onClick.AddListener(RestartGame);
     }
     public void setEndScore(int score){
         endscore = score;
     }
-    void RestartGame()
-    {
-        SceneManager.LoadScene("selectSong");
-    }
+    // void RestartGame()
+    // {
+    //     SceneManager.LoadScene("selectSong");
+    // }
 }
